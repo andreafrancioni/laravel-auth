86502184681ab1c915f4575eeeb0b7d9f93f2d96
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-31 18:34:44 +0100

    debug button css

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -46,7 +46,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        return view("admin.projects.show", compact("project"));
     }
 
     /**
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,9 +8,28 @@
                     <div class="card">
                         <div class="card-header">{{ $project->title }}</div>
                         <div class="card-body">{{ $project->description }}</div>
+                        <div class="buttons d-flex">
+                            <button type="button" class="btn btn-primary" href="{{route('admin.projects.show', $project->id)}}">Details</button>
+                            <button type="button" class="btn btn-success" href="{{route('admin.projects.edit', $project->id)}}">Edit</button>
+                            <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form>
+                        </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
 @endsection
+
+<style>
+
+    .buttons{
+        padding: 10px
+    }
+    form{
+        margin-block-end: 0!important;
+    }
+</style>
