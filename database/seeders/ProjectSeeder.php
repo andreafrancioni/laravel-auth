60429514937e9573705dd36d576f1d5376b93d11
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-31 16:44:16 +0100

    visualizza le card sotto la vista index in dashboard

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,36 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $projects = [
+            [
+                'title' => 'progetto1test',
+                'description' => 'progetto1test',
+                'thumb' => 'progetto1test',
+                'creation_date' => '2024/01/01',
+                'type' => 'progetto1test',
+            ],
+            [
+                'title' => 'progetto2test',
+                'description' => 'progetto2test',
+                'thumb' => 'progetto2test',
+                'creation_date' => '2024/01/01',
+                'type' => 'progetto2test',
+            ],
+            [
+                'title' => 'progetto3test',
+                'description' => 'progetto3test',
+                'thumb' => 'progetto3test',
+                'creation_date' => '2024/01/01',
+                'type' => 'progetto3test',
+            ],
+        ];
+
+        foreach ($projects as $project) {
+
+            $newproject = new Project();
+            $newproject->fill($project);
+            $newproject->save();
+        }
     }
 }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,1 +1,16 @@
 @extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center">
+            @foreach ($projects as $project)
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">{{ $project->title }}</div>
+                        <div class="card-body">{{ $project->description }}</div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
